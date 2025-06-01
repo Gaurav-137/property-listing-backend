3a33690e3f54dc65318c4b0a70ee47645dda025c
Gaurav Lad <86229653+Gaurav-137@users.noreply.github.com>
2025-06-02 04:30:49 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import app from './app';
 import connectDB from './config/db';
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 connectDB();
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
