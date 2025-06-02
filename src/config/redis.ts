5309e9ba60a12e41ead034440afa483aa0e0254c
Gaurav-137 <Gaurav-137@users.noreply.github.com>
2025-06-02 05:32:04 +0530

    🔀 Merge GitHub and local changes

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const redis = createClient({
-  url: process.env.REDIS_URL || 'rediss://default:********@divine-stingray-44339.upstash.io:6379'
+  url: process.env.REDIS_URL || 'rediss://default:Aa0zAAIjcDExZmZmMGRlODJjZjU0ZDAwYjZjNzEwMWRmZjQ1MWU3ZHAxMA@divine-stingray-44339.upstash.io:6379'
 });
 
 redis.on('error', (err) => console.error('Redis Client Error', err));
